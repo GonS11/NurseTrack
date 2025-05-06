6b5dd53372d2aef37cf24c5bacae448719d09c1f
Gonzalo <148366975+GonS11@users.noreply.github.com>
2025-05-06 10:18:33 +0200

    Update NurseRequestController.java

--- nurse-track-backend/src/main/java/com/nursetrack/web/controller/nurse/NurseRequestController.java
+++ nurse-track-backend/src/main/java/com/nursetrack/web/controller/nurse/NurseRequestController.java
@@ -1,4 +1,28 @@
 package com.nursetrack.web.controller.nurse;
 
-public class NurseRequestController {
+import com.nursetrack.domain.model.User;
+import com.nursetrack.service.DepartmentService;
+import com.nursetrack.service.NurseDepartmentService;
+import com.nursetrack.service.ShiftService;
+import com.nursetrack.web.dto.response.DepartmentResponse;
+import com.nursetrack.web.dto.response.ShiftResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.nio.file.AccessDeniedException;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/nurses/requests")
+@RequiredArgsConstructor
+@PreAuthorize("hasRole('NURSE')")
+public class NurseRequestController 
+{
+    //Peticiones para solicitar vacaciones y cambio de turno
 }
