920f8e222aafb5bb31e74ca0914624ee80b65768
Gonzalo <148366975+GonS11@users.noreply.github.com>
2025-05-06 10:20:14 +0200

    Update SupervisorRequestController.java

--- nurse-track-backend/src/main/java/com/nursetrack/web/controller/supervisor/SupervisorRequestController.java
+++ nurse-track-backend/src/main/java/com/nursetrack/web/controller/supervisor/SupervisorRequestController.java
@@ -1,4 +1,32 @@
 package com.nursetrack.web.controller.supervisor;
 
-public class SupervisorRequestController {
+import com.nursetrack.domain.model.User;
+import com.nursetrack.service.DepartmentService;
+import com.nursetrack.service.NurseDepartmentService;
+import com.nursetrack.service.ShiftService;
+import com.nursetrack.service.SupervisorDepartmentService;
+import com.nursetrack.web.dto.request.nurseDepartment.AssignNurseRequest;
+import com.nursetrack.web.dto.request.shift.CreateShiftRequest;
+import com.nursetrack.web.dto.request.shift.UpdateShiftRequest;
+import com.nursetrack.web.dto.response.DepartmentResponse;
+import com.nursetrack.web.dto.response.NurseDepartmentResponse;
+import com.nursetrack.web.dto.response.ShiftResponse;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+import java.nio.file.AccessDeniedException;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/supervisor/departments/{departmentId}/requests")
+@RequiredArgsConstructor
+@PreAuthorize("hasRole('SUPERVISOR')")
+public class SupervisorRequestController 
+{
+  //Acciones de ver todas las peticiones realizadas, accion de aceptar/rechazar alguna peticion
 }
