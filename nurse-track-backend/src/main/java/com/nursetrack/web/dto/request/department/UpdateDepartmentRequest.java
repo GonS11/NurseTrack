c6c36fc8cd1b7ad9bb74d2dfe6234a82cce9ece8
Gonzalo <148366975+GonS11@users.noreply.github.com>
2025-05-07 10:53:45 +0200

    Update UpdateDepartmentRequest.java

--- nurse-track-backend/src/main/java/com/nursetrack/web/dto/request/department/UpdateDepartmentRequest.java
+++ nurse-track-backend/src/main/java/com/nursetrack/web/dto/request/department/UpdateDepartmentRequest.java
@@ -9,13 +9,13 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
-public class UpdateDepartmentRequest
+public class UpdateDepartmentRequest 
 {
-    @Size(min = 2, max = 100)
+    @Size(min = 2, max = 100, message = "Department name must be between 2 and 100 characters")
     @ValidDepartmentName
     private String name;
 
-    @Size(min = 2, max = 100)
+    @Size(min = 2, max = 100, message = "Location must be between 2 and 100 characters")
     private String location;
 
     private Boolean isActive;
